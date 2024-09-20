5a3eadf7da244cb08ed3df8934e7fb4b6d03297f
Vinícius Maeda <viniciusmaeda@gmail.com>
2024-09-20 11:24:10 -0400

    servidor para calcular dois numeros

--- 2-node-http-nodemon/server-calc.js
+++ 2-node-http-nodemon/server-calc.js
@@ -1,3 +1,7 @@
+/*
+* Servidor que realiza operação de soma de dois parâmetros na url 
+*/
+
 // importar módulo/bibliotecas
 const http = require('http');
 const url = require('url');
@@ -10,18 +14,21 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   // 2 instruções de configuração do servidor (navegador)
   res.statusCode = 200; // status ok
-  res.setHeader('Content-Type', 'text/plain; charset=utf-8'); // tipo do conteúdo
+  res.setHeader('Content-Type', 'text/html; charset=utf-8'); // tipo do conteúdo (formato HTML)
 
   let q = url.parse(req.url, true).query; // consultando a string da url
   
   // exemplo de url 
-  // http://127.0.0.1:3000/?nome=vinicius&sobrenome=maeda&email=vinicius@email.com
+  // http://127.0.0.1:3000/?n1=2&n2=3
 
   // montagem da mensagem de retorno (template string)
+  let n1 = Number(q.n1);
+  let n2 = Number(q.n2);
   let txt = `
-    Nome: ${q.nome}
-    Sobrenome: ${q.sobrenome}
-    E-mail: ${q.email}
+    A soma de ${n1} + ${n2} = <b>${n1 + n2}</b><br>
+    A subtração de ${n1} - ${n2} = <b>${n1 - n2}</b><br>
+    A divisão de ${n1} / ${n2} = <b>${n1 / n2}</b><br>
+    A multiplicação de ${n1} * ${n2} = <b>${n1 * n2}</b>
   `;
 
   res.write(txt); // mensagem no <body> da página
