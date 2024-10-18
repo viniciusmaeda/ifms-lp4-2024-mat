bb23507442690734c64f6bcabd9591390c3957c7
Vinícius Maeda <viniciusmaeda@gmail.com>
2024-10-18 11:01:00 -0400

    exemplo de rotas

--- 2-node-http-nodemon/server-rotas.js
+++ 2-node-http-nodemon/server-rotas.js
@@ -12,24 +12,48 @@ const port = 3000;
 // configuração do servidor
 const server = http.createServer((req, res) => {
 
-  // configuração do header
-  res.writeHead(200, { 'Content-Type' : 'text/html; charset=utf-8' });
-
   const url = req.url;
 
   // manipulação das rotas do site
   if (url == '/') {
+    // configuração do header
+    res.writeHead(200, { 'Content-Type' : 'text/html; charset=utf-8' });
+    
     res.write('Você está na rota <b>raiz</b> do site.');
+    res.end();
+
   } else if (url == '/contato') {
+    // configuração do header
+    res.writeHead(200, { 'Content-Type' : 'text/html; charset=utf-8' });
+    
     res.write('Você está na rota <b>contato</b> do site.');
+    res.end();
+
   } else if (url == '/sobre') {
+    // configuração do header
+    res.writeHead(200, { 'Content-Type' : 'text/html; charset=utf-8' });
+    
     res.write('Você está na rota <b>sobre</b> do site.');
+    res.end();
+
+  } else if (url == '/api') {
+    // configuração do header
+    res.writeHead(200, { 'Content-Type' : 'application/json; charset=utf-8' });
+
+    res.end(JSON.stringify({
+      nome: 'Vinícius',
+      email: 'vinicius@email.com'
+    }));
+
   } else {
+    // configuração do header
+    res.writeHead(404, { 'Content-Type' : 'text/plain; charset=utf-8' });
+    
     res.write('Esta página não existe.');
+    res.end();
   }
 
 
-  res.end();
 
 });
 
